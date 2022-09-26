51ff2d62e495fa019cffb656a33c4e8ed91dc614
H-27 <jenshuth@t-online.de>
2022-09-26 16:27:50 +0200

    added kernel initializer zeros to actor

--- gym_multifrozenlake/agents/Actor.py
+++ gym_multifrozenlake/agents/Actor.py
@@ -6,11 +6,11 @@ class Actor(tf.keras.Model):
     def __init__(self, number_actions: int, number_observations: int, size_first_layer: int, size_second_layer: int, size_third_layer: int = 0):
 
         super(Actor, self).__init__()       
-        self.first_layer = tf.keras.layers.Dense(size_first_layer, activation = tf.nn.relu)
-        self.second_layer = tf.keras.layers.Dense(size_second_layer, activation = tf.nn.relu)
+        self.first_layer = tf.keras.layers.Dense(size_first_layer, activation = tf.nn.relu, kernel_initializer = 'zeros')
+        self.second_layer = tf.keras.layers.Dense(size_second_layer, activation = tf.nn.relu, kernel_initializer = 'zeros')
         if (size_third_layer > 0):
-            self.third_layer = tf.keras.layers.Dense(size_third_layer, activation = tf.nn.relu)
-        self.output_layer = tf.keras.layers.Dense(number_actions, activation = tf.nn.softmax)
+            self.third_layer = tf.keras.layers.Dense(size_third_layer, activation = tf.nn.relu, kernel_initializer = 'zeros')
+        self.output_layer = tf.keras.layers.Dense(number_actions, activation = tf.nn.softmax, kernel_initializer = 'zeros')
 
     def call(self, input):
         
